bad7bb4d6b11bc769b62adeb6747028c561bf3c6
Jake <jakeadmire1@gmail.com>
2019-05-07 12:11:31 -0600

    Set up Header component wireframe

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nav } from '../Nav/Nav';
+
+export function Header() {
+
+  return (
+    <div className="Header">
+      <h1>PalettePicker</h1>
+      <Nav />
+    </div>
+  )
+}
