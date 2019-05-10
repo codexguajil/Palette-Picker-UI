d7ebcb9d89ad482b4b63f80031efabe652656093
Jake <jakeadmire1@gmail.com>
2019-05-10 10:16:23 -0600

    Move navigation checkbox from Nav component to App component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 
 export class Nav extends Component {
 
-  toggleCheckbox = (e) => {
-    this.props.changeState({ activeSidebar: e.target.checked });
-  } 
-
   render() {
     return (
-      <div className="Nav">
+      <div className={`Nav ${this.props.activeNav ? 'active' : ''}`}>
         <div className="tutorial">
           <p>Press the ‘spacebar’ key to generate a new palette</p>
-        </div>
-        <div className="toolbar">
-          <input onChange={this.toggleCheckbox} type="checkbox" />
         </div>
       </div>
     )
