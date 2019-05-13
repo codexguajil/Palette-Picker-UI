195a57a9370647c3e6de67efd37f73cfda66558b
Jake <jakeadmire1@gmail.com>
2019-05-13 09:25:06 -0600

    Test Header component labelClassBuilder method returns

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import arrow from '../../media/arrow.svg';
 
-export function Header(props) {
+export class Header extends Component {
 
-  let labelStyle = {backgroundImage: `url(${arrow})`}
+  labelClassBuilder(boolean) {
+    return `label ${boolean ? 'flipped' : ''}`;
+  }
 
-  return (
-    <div className="Header">
-      <h1>PalettePicker</h1>
-      <label style={labelStyle} for="toggle" className={`label ${props.activeNav ? 'flipped' : ''}`} ></label>
-      <input id="toggle" onChange={props.toggleNav} type="checkbox" />
-    </div>
-  )
+  render() {
+    let labelStyle = {backgroundImage: `url(${arrow})`};
+    let labelClass = this.labelClassBuilder(this.props.activeNav);
+    return (
+      <div className="Header">
+        <h1>PalettePicker</h1>
+        <label style={labelStyle} htmlFor="toggle" className={labelClass} ></label>
+        <input id="toggle" onChange={this.props.toggleNav} type="checkbox" />
+      </div>
+    )
+  }
 }
