c6130fd5ef4e6537a87fc2f1de1fd32fa4ebd267
Jake <jakeadmire1@gmail.com>
2019-05-15 17:29:47 -0600

    Convert multiple fetch methods into one dynamic method within api.js
    Co-authored-by: Edgar Munoz <edgareloymunoz@gmail.com>

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,43 +1,16 @@
-export const fetchApiData = async (endpoint) => {
-  const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/${endpoint}`);
-  if(response.ok) {
-    const data = await response.json();
-    return data;
+export const fetchApiData = async (endpoint, options) => {
+  const url = process.env.REACT_APP_BACKEND_URL + `/api/v1/${endpoint}`;
+  const response = await fetch(url, options);
 
-  } else {
-    throw Error(response.statusText);
-  }
-  
-}
+  switch(options.method) {
+    case('DELETE') :
+      return response;
 
-export const postApiData = async (endpoint, data) => {
-  const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/${endpoint}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  });
+    case('PATCH') :
+      return response.json();
 
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-
-  } else {
-    throw Error(response.statusText);
-  }
-}
-
-export const deleteApiData = async (endpoint) => {
-  const response = await fetch(process.env.REACT_APP_BACKEND_URL + `/api/v1/${endpoint}`, {
-    method: 'DELETE'
-  });
-
-  if (response.ok) {
-    // console.log(response);
-    return response.statusText;
-    // const data = await response.json();
-    // return data;
-
-  } else {
-    throw Error(response.statusText);
+    default :
+      const data = await response.json();
+      return data
   }
 }
